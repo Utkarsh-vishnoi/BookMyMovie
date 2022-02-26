d72cf0fd02c9cf2c7e04e2c4987fc69ec28362bd
Utkarsh Vishnoi <utkarshvishnoi25@gmail.com>
2022-02-27 00:48:36 +0530

    Added Upcoming Movies Grid List

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,8 +1,65 @@
-import React from 'react'
+import { GridList, GridListTile, GridListTileBar, IconButton } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import React, { useEffect, useState } from 'react'
+
+import Header from "../../common/header/Header"
+import Heading from "../../common/heading/Heading"
+
+import "./Home.css"
+
+const useStyles = makeStyles({
+    root: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+        overflow: "hidden",
+        marginBottom: "15px",
+    },
+    gridList: {
+        flexWrap: "nowrap !important",
+        transform: "translateZ(0)",
+    },
+
+    titleBar: {
+        background:
+            "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+    },
+})
 
 const Home = () => {
+
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        fetch("/api/v1/movies")
+            .then((response) => response.json())
+            .then((payload) => setMovies(payload.movies))
+    })
+
+    const classes = useStyles()
+
     return (
-        <div>Home</div>
+        <React.Fragment>
+            <Header bookShow={false}></Header>
+            <Heading />
+            <div className={classes.root}>
+                <GridList className={classes.gridList} cols={6} cellHeight={250}>
+                    {movies.map((tile) => (
+                        <GridListTile key={tile.id}>
+                            <img src={tile.poster_url} alt={tile.title} />
+                            <GridListTileBar
+                                title={tile.title}
+                                classes={{
+                                    root: classes.titleBar,
+                                    title: classes.title,
+                                }}
+                                actionIcon={<IconButton aria-label={`star ${tile.title}`} />}
+                            />
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </div>
+        </React.Fragment>
     )
 }
 
