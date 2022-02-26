a4b7f8ca5d372add8822db5be3a4a7d3432f6d28
Utkarsh Vishnoi <utkarshvishnoi25@gmail.com>
2022-02-26 14:47:39 +0530

    Added Validation Logic for Login and Sign Up Forms

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,4 +1,5 @@
-import { Button, Tab, Tabs, TextField, Typography } from '@material-ui/core'
+import { Button, Tab, Tabs, Typography } from '@material-ui/core'
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator'
 import React, { useState } from 'react'
 import Modal from "react-modal"
 import { Link } from 'react-router-dom'
@@ -8,15 +9,30 @@ import TabContent from "../tabContent/TabContent"
 import './Header.css'
 
 const Header = ({ bookShowID, bookShow }) => {
+
     const [isLoggedIn, setIsLoggedIn] = useState(false)
-    const [loginOpen, setLoginOpen] = useState(false)
+    const [loginOpen, setLoginOpen] = useState(true)
     const [activeTab, setActiveTab] = useState(0)
     const [registered, setRegistered] = useState(false)
 
+    const [firstName, setFirstname] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [contactNo, setContactNo] = useState("")
+
+    const [userName, setUserName] = useState("")
+
     const loginFormHandler = () => {
+        console.log("Log In!")
         setIsLoggedIn(false)
         setLoginOpen(false)
-    };
+    }
+
+    const signupFormHandler = () => {
+        console.log("Sign UP!")
+        setRegistered(true)
+    }
 
     return (
         <React.Fragment>
@@ -45,6 +61,7 @@ const Header = ({ bookShowID, bookShow }) => {
             <Modal
                 isOpen={loginOpen}
                 onRequestClose={() => setLoginOpen(false)}
+                ariaHideApp={false}
                 style={{
                     overlay: {
                         position: "fixed",
@@ -76,52 +93,89 @@ const Header = ({ bookShowID, bookShow }) => {
                     <Tab label="Register" />
                 </Tabs>
                 <TabContent activeTab={activeTab} tabIndex={0}>
-                    <TextField label="Username" required style={{ margin: "5px 0px" }} />
-                    <TextField
-                        label="Password"
-                        type="password"
-                        style={{ margin: "5px 0px" }}
-                        required
-                    />
-                    <div>
-                        <Button
-                            variant="contained"
-                            onClick={loginFormHandler}
-                            color="primary"
-                            style={{ margin: "20px 20px" }}
-                        >Login</Button>
-                    </div>
+                    <ValidatorForm onSubmit={loginFormHandler}>
+                        <TextValidator
+                            label="Username"
+                            style={{ margin: "5px 0px" }}
+                            value={userName}
+                            onChange={(event) => setUserName(event.target.value)}
+                            validators={['required']}
+                            errorMessages={['required']}
+                        />
+                        <TextValidator
+                            label="Password"
+                            type="password"
+                            style={{ margin: "5px 0px" }}
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)}
+                            validators={['required']}
+                            errorMessages={['required']}
+                        />
+                        <div>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                type="submit"
+                                style={{ margin: "20px 20px" }}
+                            >Login</Button>
+                        </div>
+                    </ValidatorForm>
                 </TabContent>
                 <TabContent activeTab={activeTab} tabIndex={1}>
-                    <TextField
-                        label="First Name"
-                        required
-                        style={{ margin: "5px 0px" }}
-                    />
-                    <TextField label="Last Name" required style={{ margin: "5px 0px" }} />
-                    <TextField label="Email" required style={{ margin: "5px 0px" }} />
-                    <TextField
-                        label="Password"
-                        required
-                        type="password"
-                        style={{ margin: "5px 0px" }}
-                    />
-                    <TextField
-                        label="Contact No"
-                        required
-                        style={{ margin: "5px 0px" }}
-                    />
-                    {registered ? (
-                        <Typography variant="subtitle1" gutterBottom>
-                            Registration Successful. Please Login!
-                        </Typography>
-                    ) : null}
-                    <Button
-                        variant="contained"
-                        onClick={() => setRegistered(true)}
-                        color="primary"
-                        style={{ margin: "20px 20px" }}
-                    >Register</Button>
+                    <ValidatorForm
+                        onSubmit={signupFormHandler}>
+                        <TextValidator
+                            label="First Name"
+                            style={{ margin: "5px 0px" }}
+                            value={firstName}
+                            onChange={(event) => setFirstname(event.target.value)}
+                            validators={['required']}
+                            errorMessages={['required']}
+                        />
+                        <TextValidator
+                            label="Last Name"
+                            style={{ margin: "5px 0px" }}
+                            value={lastName}
+                            onChange={(event) => setLastName(event.target.value)}
+                            validators={['required']}
+                            errorMessages={['required']}
+                        />
+                        <TextValidator
+                            label="Email"
+                            style={{ margin: "5px 0px" }}
+                            value={email}
+                            onChange={(event) => setEmail(event.target.value)}
+                            validators={['required', 'isEmail']}
+                            errorMessages={['required', 'Invalid Email']}
+                        />
+                        <TextValidator
+                            label="Password"
+                            style={{ margin: "5px 0px" }}
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)}
+                            validators={['required']}
+                            errorMessages={['required']}
+                        />
+                        <TextValidator
+                            label="Contact No"
+                            style={{ margin: "5px 0px" }}
+                            value={contactNo}
+                            onChange={(event) => setContactNo(event.target.value)}
+                            validators={['required', 'isNumber']}
+                            errorMessages={['required', 'Only Number Allowed']}
+                        />
+                        {registered ? (
+                            <Typography variant="subtitle1" gutterBottom>
+                                Registration Successful. Please Login!
+                            </Typography>
+                        ) : null}
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                            style={{ margin: "20px 20px" }}
+                        >Register</Button>
+                    </ValidatorForm>
                 </TabContent>
             </Modal>
         </React.Fragment>
