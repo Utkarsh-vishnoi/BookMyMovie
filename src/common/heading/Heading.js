dfccdd0658b81f0da5e066f590c3c2aa0d78afef
Utkarsh Vishnoi <utkarshvishnoi25@gmail.com>
2022-02-26 14:54:24 +0530

    Added Upcoming Movies Heading

---
+++ src/common/heading/Heading.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import './Heading.css'
+
+const Heading = () => {
+    return (
+        <div>
+            <p className="heading">Upcoming Movies</p>
+        </div>
+    )
+}
+
+export default Heading
